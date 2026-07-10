43f8e1f14c2a6d5dbb1e8faf2752788f28b064c3
Marckr02 <marco.rios@epn.edu.ec>
2026-07-10 08:05:26 -0500

    Add type annotations to request and response in filter

--- Proyecto_IB_Cons_SW/epn-event-manager/src/common/filters/all-exceptions.filter.ts
+++ Proyecto_IB_Cons_SW/epn-event-manager/src/common/filters/all-exceptions.filter.ts
@@ -1,13 +1,14 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus, Logger } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
   private readonly logger = new Logger(AllExceptionsFilter.name);
 
-  catch(exception: unknown, host: ArgumentsHost) {
+  catch(exception: unknown, host: ArgumentsHost): void {
     const ctx = host.switchToHttp();
-    const response = ctx.getResponse();
-    const request = ctx.getRequest();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
 
     const status = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
     const message = exception instanceof HttpException ? exception.message : 'Error interno del servidor';
